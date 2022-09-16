171b7a206e37d9950381dd9bf93d488035416391
SamandarDeveloper05 <113525304+SamandarDeveloper05@users.noreply.github.com>
2022-09-16 17:54:58 +0500

    Add files via upload

---
+++ React-Calculator/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+import Calculator from './Components/Calculator/Calculator';
+const App = () => {
+  return (
+
+    <div>
+      <h2  className='title'>Samandar's Calculator</h2>
+      <Calculator/>
+
+
+    </div>
+
+  )
+}
+
+export default App
